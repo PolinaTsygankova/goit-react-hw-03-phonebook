4fda56a78b264fbaa1e3ccc02990dd9cf7ed9c73
Polina Tsygankova <116460158+PolinaTsygankova@users.noreply.github.com>
2023-06-16 22:36:22 +0300

    Initial commit

---
+++ src/components/ContactItem/ContactItem.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import { Item, DeleteBtn, Text } from './ContactItem.styled';
+
+export function ContactItem({ name, number, id, onDelete }) {
+  return (
+    <>
+      <Item key={id}>
+        <div>
+          <Text>{name}: </Text>
+          <Text> {number}</Text>
+        </div>
+
+        <DeleteBtn type="button" onClick={() => onDelete(id)}>
+          Delete
+        </DeleteBtn>
+      </Item>
+    </>
+  );
+}
+
+ContactItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
